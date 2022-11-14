01c933aa515d186d9b6b6c4afea46f10f772f38d
Heeewoo Sung <skanr11233@naver.com>
2022-11-15 01:06:13 +0900

    '22.11.14.01:51

--- app/src/main/java/lx/com/kocoa/FestInforStatusActivity.kt
+++ app/src/main/java/lx/com/kocoa/FestInforStatusActivity.kt
@@ -6,11 +6,14 @@ import lx.com.kocoa.databinding.ActivityFestInforStatusBinding
 
 class FestInforStatusActivity : AppCompatActivity() {
     lateinit var binding:ActivityFestInforStatusBinding
+    lateinit var festMapFragment: FestMapFragment // 맵 띄우는거 땡겨올려고
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFestInforStatusBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        festMapFragment = FestMapFragment() // 맵 띄우기 위한 프래그먼트 사용
 
         //선택한 아이템 현황화면에 가져오는거
         SAppData.selelctedItem.apply {
@@ -21,6 +24,7 @@ class FestInforStatusActivity : AppCompatActivity() {
             binding.showfestName.text = "축제이름 : ${SAppData.data5}"
             binding.showfestRange.text = "축제규모 : ${SAppData.data6}"
             binding.showfestLocation.text = "개최지역 : ${SAppData.data7}"
+
         }
 
         //뒤로가기버튼
