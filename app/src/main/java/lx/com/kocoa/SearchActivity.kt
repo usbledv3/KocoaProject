c6c2252a2eb761be9510b216d0eca5e8ed3c593d
Yewon Byun <cindy971222@naver.com>
2022-11-21 20:48:12 +0900

    9:39

--- app/src/main/java/lx/com/kocoa/SearchActivity.kt
+++ app/src/main/java/lx/com/kocoa/SearchActivity.kt
@@ -1,15 +1,8 @@
 package lx.com.kocoa
 
-import android.content.ContentValues.TAG
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.widget.SearchView
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_search.*
 import lx.com.kocoa.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
@@ -23,64 +16,22 @@ class SearchActivity : AppCompatActivity() {
 
 
 
-
-
-    var searchViewTextListener: SearchView.OnQueryTextListener =
-        object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(filterString: String?): Boolean {
-                searchAdapter!!.filter.filter(filterString)
-                return true
-            }
-
-            override fun onQueryTextChange(filterString: String?): Boolean {
-                searchAdapter!!.filter.filter(filterString)
-                return true
-            }
-
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val searchView = binding.searchView
-        searchView.setOnQueryTextListener(searchViewTextListener)
+        supportFragmentManager.beginTransaction().replace(R.id.searchContainer, SearchFragment()).commit()
 
-        initList()
-    }
+            binding.searchButton2.setOnClickListener {
+                if(binding.searchInput.text.toString() == "2022") {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.searchContainer, AfterSearchFragment()).commit()
+                }
+            }
 
-    fun initList() {
-        // 리스트의 모양을 담당하는 것
-        // (LinearLayoutManager : 아래쪽으로 아이템들이 보이는 것, GridLayoutManager: 격자 형태로 보이는 것)
-        val layoutManager = LinearLayoutManager(this)
-        binding.searchList.layoutManager = layoutManager
-
-        // 2. 어댑터를 설정하는 것
-        // 실제 데이터를 관리하고 각 아이템의 모양을 만들어주는 것
-        searchAdapter = SearchAdapter()
-        binding.searchList.adapter = searchAdapter
-
-        // 3. 테스트로 아이템을 위한 데이터 넣어보기
-        searchAdapter?.apply {
-            this.items.add(SearchData("2022 서울 빛초롱축제", R.drawable.festival1,"2022.12.16-12.31", "서울시 청계천"))
-            this.items.add(SearchData("2023 화천 산천어축제", R.drawable.hwachun_festival,"2023.1.7-1.29", "강원도 화천"))
-            this.items.add(SearchData("2022 노들섬 뮤직페스티벌", R.drawable.nodeul_fes,"2022.10.13-10.16", "서울시 노들섬"))
-        }
-
-//        // 4. 아이템을 클릭했을 때 동작할 코드 넣어주기
-//        searchAdapter?.listner = object: OnSearchItemClickListner {
-//            override fun onItemClick(holder: SearchAdapter.ViewHolder?, view: View?, position: Int) {
-//                searchAdapter?.apply {
-//                    val item = items.get(position)
-//
-//                    AppData.selectedItem = item
-//
-//                }
-//
-//            }
-//
-//        }
 
     }
+
+
 }
---
+++ app/src/main/java/lx/com/kocoa/SearchFragment.kt
@@ -0,0 +1,66 @@
+package lx.com.kocoa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import lx.com.kocoa.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    var _binding: FragmentSearchBinding? = null
+    val binding get() = _binding!!
+
+    var searchAdapter:SearchAdapter? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        initView()
+
+        return binding.root
+    }
+
+    fun initView() {
+        // 리스트의 모양을 담당하는 것
+        // (LinearLayoutManager : 아래쪽으로 아이템들이 보이는 것, GridLayoutManager: 격자 형태로 보이는 것)
+        val layoutManager = LinearLayoutManager(context)
+        binding.searchList1.layoutManager = layoutManager
+
+        // 2. 어댑터를 설정하는 것
+        // 실제 데이터를 관리하고 각 아이템의 모양을 만들어주는 것
+        searchAdapter = SearchAdapter()
+        binding.searchList1.adapter = searchAdapter
+
+        // 3. 테스트로 아이템을 위한 데이터 넣어보기
+        searchAdapter?.apply {
+            this.items.clear()
+            this.items.add(
+                SearchData(
+                    "마노르블랑 핑크뮬리축제", R.drawable.pinkmuli_fes,"2021.09.13-11.30", "제주도 서귀포시 안덕면 일주서로"
+                )
+            )
+            this.items.add(
+                SearchData(
+                    "펫츠런(Pets Run) 고양",
+                    R.drawable.petsrun_fes,
+                    "2021.12.2-12.3",
+                    "경기도 고양시 일산동구 중앙로 1271-4"
+                )
+            )
+            this.items.add(
+                SearchData(
+                    "숲 속 모두의, 포레포레",
+                    R.drawable.fore_fes,
+                    "2021.8.27-11.26",
+                    "경기도 수원시 권선구 서둔로 166"
+                )
+            )
+        }
+
+    }
+}
