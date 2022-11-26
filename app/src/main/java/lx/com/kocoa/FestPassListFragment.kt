e68f17f2f79dba0879b022346356b5b79ab422af
Heeewoo Sung <skanr11233@naver.com>
2022-11-26 13:50:53 +0900

    '22.11.14.01:51

--- app/src/main/java/lx/com/kocoa/FestPassListFragment.kt
+++ app/src/main/java/lx/com/kocoa/FestPassListFragment.kt
@@ -1,17 +1,22 @@
 package lx.com.kocoa
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import lx.com.kocoa.databinding.FragmentFestPassListBinding
 
 class FestPassListFragment : Fragment() {
     var _binding : FragmentFestPassListBinding? = null
     val binding get() = _binding!!
-    var miniAdapter:MiniAdapter? = null
+    var festAdapter:FestAdapter? = null
+    val FestPassLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentFestPassListBinding.inflate(inflater, container, false)
 
@@ -26,24 +31,29 @@ class FestPassListFragment : Fragment() {
     fun initList() {
         // 리스트의 모양을 담당하는 것
         // (LinearLayoutManager : 아래쪽으로 아이템들이 보이는 것, GridLayoutManager: 격자 형태로 보이는 것)
-        val layoutManager = LinearLayoutManager(MainActivity())
+        val layoutManager = LinearLayoutManager(activity)
         binding.makepassList.layoutManager = layoutManager
 
         // 2. 어댑터를 설정하는 것
         // 실제 데이터를 관리하고 각 아이템의 모양을 만들어주는 것
-        miniAdapter = MiniAdapter()
-        binding.makepassList.adapter = miniAdapter
+        festAdapter = FestAdapter()
+        binding.makepassList.adapter = festAdapter
 
-        miniAdapter?.apply {
-            GamesData.game1?.let { items.add(it) }
-            GamesData.game2?.let { items.add(it) }
+        festAdapter?.apply {
+            this.items.add(FestManagerData("패스등록예시","","적용인원예시","적용기간예시","축제이름"))
         }
 
         // 4. 아이템을 클릭했을 때 동작할 코드 넣어주기
-        miniAdapter?.listener = object: OnMiniItemClickListener {
-            override fun onItemClick(holder: MiniAdapter.ViewHolder, view: View?, position: Int) {
-                miniAdapter?.apply {
-                    val item = items.get(position)
+        festAdapter?.listener = object:OnFestInforItemClickListener {
+            override fun onItemClick(holder: FestAdapter.ViewHolder?, view: View?, position: Int) {
+                festAdapter?.apply {
+                    val item2 = items.get(position)
+
+                    SAppData.selelctedItem = item2
+                    activity?.let {
+                        val FestPassIntent = Intent(it, FestPassFragment::class.java)
+                        FestPassLauncher.launch(FestPassIntent)
+                    }
                 }
             }
         }
